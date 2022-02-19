b190335346633b676466089763ec960cccc0ebc5
Marco Arena <ilpropheta@gmail.com>
2022-02-19 17:59:11 +0100

    Create source_location.cpp

---
+++ source_location.cpp
@@ -0,0 +1,20 @@
+// C++20 for the daily job (Marco Arena)
+//
+// std::source_location
+
+#include <iostream>
+#include <source_location>
+
+using namespace std;
+
+template <typename T> 
+void fun(T)
+{
+    cout << "This is line number " << source_location::current().function_name() << "\n";
+}
+
+int main()
+{
+    cout << "This is line number " << source_location::current().line() << "\n";
+    fun(10);
+}
