c946840971f21b259aafa36b6ee71f0ff4d81727
Marco Arena <ilpropheta@gmail.com>
2022-02-23 17:08:04 +0100

    Update source_location.cpp

--- source_location.cpp
+++ source_location.cpp
@@ -10,11 +10,12 @@ using namespace std;
 template <typename T> 
 void fun(T)
 {
-    cout << "This is line number " << source_location::current().function_name() << "\n";
+    cout << "This is line number: " << source_location::current().function_name() << "\n";
 }
 
 int main()
 {
-    cout << "This is line number " << source_location::current().line() << "\n";
+    cout << "This is line number: " << source_location::current().line() << "\n";
+    cout << "This is function name: " << source_location::current().function_name() << "\n";
     fun(10);
 }
